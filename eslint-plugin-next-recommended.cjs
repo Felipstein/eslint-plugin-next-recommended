8d0ffd50907b0c5c05f25920e048f1e569e341a8
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-10-16 18:27:22 -0300

    chore: remove rule prefix name

--- eslint-plugin-next-recommended.cjs
+++ eslint-plugin-next-recommended.cjs
@@ -1,11 +1,11 @@
 const unnecessarilyClientDeclaration = require("./rules/unnecessarily-client-declaration.cjs");
-const requireUseClientRule = require("./rules/require-use-client.cjs");
+const requireUseClient = require("./rules/require-use-client.cjs");
 const asyncComponentNoHooks = require("./rules/async-component-no-hooks.cjs");
 
 module.exports = {
   rules: {
     'unnecessarily-client-declaration': unnecessarilyClientDeclaration,
-    'require-use-client': requireUseClientRule,
+    'require-use-client': requireUseClient,
     'async-component-no-hooks': asyncComponentNoHooks,
   },
 };
