fac8ad2520e66831d63121d04b4e76b9cc2aa388
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-10-17 09:40:18 -0300

    chore: add util link to require-use-client rule

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 - Rules para server actions: (já verificar se a função declarada como 'use server' é uma promise)
-- Linkar referências do next para as regras, por exemplo: "See: next-link"
--- rules/require-use-client.cjs
+++ rules/require-use-client.cjs
@@ -5,7 +5,7 @@ module.exports = {
     type: 'problem',
 
     docs: {
-      description: 'Hooks and interactivity are only available for files declared as "use client"',
+      description: 'Hooks and interactivity are only available for files declared as "use client". See: https://nextjs.org/docs/app/building-your-application/rendering/client-components#using-client-components-in-nextjs',
     },
 
     fixable: 'code',
@@ -28,7 +28,7 @@ module.exports = {
         if(isHookCall(node) && !useClientIsDeclared) {
           context.report({
             node,
-            message: `You must declare 'use client' at the beginning of this file before using hooks inside a component. Hooks (like ${node.callee.name}) only work on client-side components.`,
+            message: `You must declare 'use client' at the beginning of this file before using hooks inside a component. Hooks (like ${node.callee.name}) only work on client-side components. See: https://nextjs.org/docs/app/building-your-application/rendering/client-components#using-client-components-in-nextjs`,
             fix: (fixer) => (
               fixer.insertTextBefore(firstStatement, "'use client'\n\n")
             )
@@ -40,7 +40,7 @@ module.exports = {
         if(isInteractivityAttribute(node) && !useClientIsDeclared) {
           context.report({
             node,
-            message: `You must declare 'use client' at the beginning of this file before using interactivity inside a component. Interactivity (like ${node.name}) only work on client-side components.`,
+            message: `You must declare 'use client' at the beginning of this file before using interactivity inside a component. Interactivity (like ${node.name}) only work on client-side components. See: https://nextjs.org/docs/app/building-your-application/rendering/client-components#using-client-components-in-nextjs`,
             fix: (fixer) => (
               fixer.insertTextBefore(firstStatement, "'use client'\n\n")
             )
